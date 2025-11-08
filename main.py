58222e05816b5d3787d362d1a1d9be1140db4b35
Desaboina Krishna Sai <42516247+KrrishDDVKS@users.noreply.github.com>
2025-11-08 17:09:55 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv,dotenv_values
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.vectorstores import VectorStoreRetriever
-from langchain-community.chains import RetrievalQA
+from langchain_community.chains import RetrievalQA
 from langchain.chains import LLMChain
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
@@ -397,6 +397,7 @@ if uploaded:
         st.chat_message("assistant").write(answer)
 if st.button('clear'):
     h.update_one({"id": 'krrish'},{"$set": {"text": ""}})
+
 
 
 
